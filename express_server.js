5485f2f0d7ed7fa819ca9484d2314a1a61b6de95
NickAzz <nick.azurin@gmail.com>
2021-05-10 23:54:09 +0000

    added login and logout functionality based on cookies

--- express_server.js
+++ express_server.js
@@ -2,15 +2,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const cookieParser = require(`cookie-parser`);
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
-  "dhWui4": "http://www.bing.com"
 };
 
 //SERVER REQUESTS
@@ -31,29 +30,44 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.post('/urls/:shortURL/edit', (req, res) => {
-  console.log("reach");
   let shortURL = req.params.shortURL;
   res.redirect(`/urls/${shortURL}`);
   res.end();
 });
 
+app.post('/login', (req, res) => {
+  const user = req.body.username;
+  res.cookie('username', user);
+  res.redirect(301, `/urls`);
+  res.end();
+});
+
+app.post('/logout/:username', (req, res) => {
+  res.clearCookie('username');
+  res.redirect(301, `/urls`);
+  res.end();
+});
+
 //SERVER PAGES
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
 app.get('/urls', (req, res) => {
-  const dataVars = {urls: urlDatabase};
+  const currentUser = req.cookies.username;
+  const dataVars = {username: currentUser, urls: urlDatabase};
   res.render('urls_index', dataVars);
 });
 
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+app.get('/urls/new', (req, res) => {;
+  const currentUser = req.cookies.username;
+  res.render('urls_new', {username: currentUser});
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const dataVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  // res.redirect(dataVars.longURL);
+  const currentUser = req.cookies.username;
+  const dataVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: currentUser};
   res.render('urls_show', dataVars);
 });
 
