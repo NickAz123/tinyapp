5f54464db1eb15c33f02b4fdd5e4b66165655ec1
NickAzz <nick.azurin@gmail.com>
2021-05-10 19:34:15 +0000

    added /urls request and passed in urlDatabase

--- express_server.js
+++ express_server.js
@@ -3,17 +3,15 @@ const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/urls', (req, res) => {
+  const dataVars = {urls: urlDatabase};
+  res.render("urls_index", dataVars);
 });
 
 app.get("/", (req, res) => {
